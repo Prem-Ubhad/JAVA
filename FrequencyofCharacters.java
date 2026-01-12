f8f49a072054e0dd6a2d85cfa18e4160650806c3
Prem Ubhad <coderprem0@gmail.com>
2026-01-13 00:21:53 +0530

    commit

--- FrequencyofCharacters.java
+++ FrequencyofCharacters.java
@@ -1,3 +1,20 @@
+import java.util.HashMap;
+
 class FrequencyofCharacters {
-    
+    public static void main(String[] args) {
+        String s = "AbcDA";
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch =s.charAt(i);
+            if(map.containsKey(ch)){
+                int count = map.get(ch);
+                map.put(ch, count+1);
+            }
+            else{
+                map.put(ch, 1);
+            }
+
+            }
+            System.out.println(map);
+        }
 }
