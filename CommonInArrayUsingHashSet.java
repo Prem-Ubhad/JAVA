e37a9e2e86db21fcdfb785c95395a96e0d796149
Prem Ubhad <coderprem0@gmail.com>
2026-01-15 15:08:13 +0530

    commit

---
+++ CommonInArrayUsingHashSet.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class CommonInArrayUsingHashSet {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {3, 4, 5, 6, 7};
+
+        // Convert first array to HashSet
+        Set<Integer> set = new HashSet<>();
+        for (int num : arr1) {
+            set.add(num);
+        }
+
+        // Find common elements
+        Set<Integer> common = new HashSet<>();
+        for (int num : arr2) {
+            if (set.contains(num)) {
+                common.add(num);
+            }
+        }
+
+        // Print common elements
+        System.out.println("Common elements: " + common);
+    }
+}
