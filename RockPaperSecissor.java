b591144632dc651035d2d2ffdb10d2934b1cd4a7
Prem Ubhad <coderprem0@gmail.com>
2026-01-17 21:27:10 +0530

    commit

--- RockPaperSecissor.java
+++ RockPaperSecissor.java
@@ -1,8 +1,14 @@
 public class RockPaperSecissor {
     public static void main(String[] args) {
         
+        //ROCK PAPER SCISSORS GAME
+
         // Declare Variables
-        // 
+        // Ask for the choice
+        // compare with the computer
+        // Show win or Loss
+        // Ask for Replay
+        // Goodbye Message
 
     }
 }
